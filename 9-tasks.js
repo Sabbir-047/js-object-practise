710b0c0e11bff107613f423cb8307fbaf41c848a
Sabbir <sabbirbhuiyan026@gmail.com>
2026-02-02 07:17:41 +0600

    finished

--- 9-tasks.js
+++ 9-tasks.js
@@ -30,13 +30,48 @@ console.log(car);
 // ========
 //Display the physics marks as output.
 const student = {
-    name: "Hamim Sakep",
-    id: 5421,
-    physics: {
-        subject: "HSC Physics",
-        author: "Shahjahan Tapan",
-        marks: 30
-    }
+  name: "Hamim Sakep",
+  id: 5421,
+  physics: {
+    subject: "HSC Physics",
+    author: "Shahjahan Tapan",
+    marks: 30,
+  },
 };
 const phyMarks = student.physics.marks;
 console.log(phyMarks);
+
+
+
+
+// Task - 4
+// ========
+// Count the number of properties
+let student1 = {
+  name: "Ariana Grande",
+  age: 21,
+  city: "Gaibandha",
+  isStudent: true,
+};
+
+const number = Object.keys(student1).length;
+console.log(number);
+
+
+
+
+
+// Task - 5
+// =========
+// Loop through an object and print the key-value pairs with their types
+let myObj = {
+  name: "John Doe",
+  age: 25,
+  city: "Example City",
+  isStudent: true,
+};
+
+const key = Object.keys(myObj);
+for (const key in myObj) {
+    console.log('key: ', key,  '|', 'type: ', typeof myObj[key]);
+}
