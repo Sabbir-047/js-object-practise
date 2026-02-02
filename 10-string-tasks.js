46b595be5f8c41aa8deb2220e7057316498d8490
Sabbir <sabbirbhuiyan026@gmail.com>
2026-02-02 17:46:48 +0600

    finished

--- 10-string-tasks.js
+++ 10-string-tasks.js
@@ -31,7 +31,7 @@ console.log(count1);
 // Task - 3
 // =========
 // Check whether a string contains all the vowels a, e, i, o, u
-let myStr = "aeiou";
+let myStr = "education";
 if (
   myStr.includes("a") &&
   myStr.includes("e") &&
@@ -43,3 +43,30 @@ if (
 } else {
   console.log("No");
 }
+
+
+
+// Task - 4
+// =========
+// If a given string has either x, replace x by y. If the given string has X, replace it by Y.
+let words = 'x-man X-man';
+if (words.includes('x') && words.includes('X')) {
+    let replaceText = words.replace('x', 'y');
+    let replaceText1 = words.replace('X', 'Y');
+    console.log(replaceText);
+    console.log(replaceText1);
+}
+
+
+
+// Task - 5
+// ========
+// Capitalize Every first letter of each word in a string
+const capitalize = 'education hello welcome';
+const word = capitalize.split(" ");
+
+for (const element of word) {
+    console.log(element[0].toUpperCase() + element.slice(1));
+}
+// console.log(word);
+
